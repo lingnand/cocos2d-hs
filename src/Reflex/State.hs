09c353f8b0137aaf4a098b2c548fb09de55e235e
Lingnan Dai <lingnan.d@gmail.com>
2016-10-01 15:05:27 +0100

    add Reflex State

---
+++ src/Reflex/State.hs
@@ -0,0 +1,170 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ImpredicativeTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE RecursiveDo #-}
+module Reflex.State
+  (
+    AccStateT
+  , DynStateT
+  , EventStateT
+  , UniqDynStateT
+  , BehaviorStateT
+  , watch
+  , watches
+  , focus
+  , refine
+  , runAccStateT
+  , execAccStateT
+  , evalAccStateT
+  , adjust
+  , adjustMaybe
+  , pnon
+  , pnon'
+  ) where
+
+
+import Control.Lens
+import Control.Monad
+import Control.Monad.Fix
+import Control.Monad.Trans.Class
+import Control.Monad.IO.Class
+import Control.Monad.Ref
+import Control.Monad.Exception
+import Reflex
+import Reflex.Host.Class
+import Reflex.Cocos2d.Class
+import Data.Maybe
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State
+
+---- generalization: AccStateT
+---- the input is always the same - like a reader
+newtype AccStateT t f s m a = AccStateT
+                            { _runAccStateT :: StateT [Event t (s -> Maybe s)] (ReaderT (f s) m) a }
+  deriving (Functor, Applicative, Monad, MonadFix, MonadIO, MonadException, MonadAsyncException)
+
+type DynStateT t = AccStateT t (Dynamic t)
+type EventStateT t = AccStateT t (Event t)
+type UniqDynStateT t = AccStateT t (UniqDynamic t)
+type BehaviorStateT t = AccStateT t (Behavior t)
+
+instance MonadTrans (AccStateT t f s) where
+    lift = AccStateT . lift . lift
+
+watch :: Monad m => AccStateT t f s m (f s)
+watch = AccStateT . lift $ ask
+
+watches :: (Functor f, Monad m) => (s -> a) -> AccStateT t f s m (f a)
+watches f = fmap f <$> watch
+
+{-# INLINE composeMaybe #-}
+composeMaybe :: (a -> Maybe a) -> (a -> Maybe a) -> (a -> Maybe a)
+composeMaybe f g a
+  | Just a' <- g a = f a'
+  | otherwise      = f a
+
+runAccStateT :: (Accumulator t f, MonadHold t m, MonadFix m) => AccStateT t f s m a -> s -> m (a, f s)
+runAccStateT ms initial = mdo
+    stateDyn <- accumMaybe (&) initial $ mergeWith composeMaybe ts
+    (a, ts) <- _runAccStateT' ms stateDyn []
+    return (a, stateDyn)
+
+_runAccStateT' :: AccStateT t f s m a -> f s -> [Event t (s -> Maybe s)] -> m (a, [Event t (s -> Maybe s)])
+_runAccStateT' ms d ts = flip runReaderT d . flip runStateT ts . _runAccStateT $ ms
+
+execAccStateT :: (Accumulator t f, MonadHold t m, MonadFix m) => AccStateT t f s m a -> s -> m (f s)
+execAccStateT ms initial = snd <$> runAccStateT ms initial
+
+evalAccStateT :: (Accumulator t f, MonadHold t m, MonadFix m) => AccStateT t f s m a -> s -> m a
+evalAccStateT ms initial = fst <$> runAccStateT ms initial
+
+focus :: (Reflex t, MonadHold t m, Functor f) => ALens' s a -> AccStateT t f a m b -> AccStateT t f s m b
+focus len am = do
+    let clonedGetter = cloneLens len
+        clonedSetter = cloneLens len
+    da <- watches (^.clonedGetter)
+    (b, ta') <- lift $ _runAccStateT' am da []
+    let !lts = clonedSetter <$> mergeWith composeMaybe ta'
+    adjustMaybe lts
+    return b
+
+refine :: (f2 a -> f1 a) -> AccStateT t f1 a m b -> AccStateT t f2 a m b
+refine f m = AccStateT . StateT $ \ts -> ReaderT $ \d -> _runAccStateT' m (f d) ts
+
+adjust :: (Reflex t, Monad m) => Event t (s -> s) -> AccStateT t f s m ()
+adjust et = let !et' = fmap Just <$> et in AccStateT $ modify (et':)
+
+adjustMaybe :: Monad m => Event t (s -> Maybe s) -> AccStateT t f s m ()
+adjustMaybe !et = AccStateT $ modify (et:)
+
+instance MonadSample t m => MonadSample t (AccStateT t f s m) where
+    sample = lift . sample
+
+instance MonadHold t m => MonadHold t (AccStateT t f s m) where
+    hold v0 = lift . hold v0
+    holdDyn v0 = lift . holdDyn v0
+    holdIncremental v0 = lift . holdIncremental v0
+
+instance MonadReflexCreateTrigger t m => MonadReflexCreateTrigger t (AccStateT t f s m) where
+    newEventWithTrigger = lift . newEventWithTrigger
+    newFanEventWithTrigger = lift . newFanEventWithTrigger
+
+instance MonadSubscribeEvent t m => MonadSubscribeEvent t (AccStateT t f s m) where
+    subscribeEvent = lift . subscribeEvent
+
+instance MonadRef m => MonadRef (AccStateT t f s m) where
+    type Ref (AccStateT t f s m) = Ref m
+    newRef = lift . newRef
+    readRef = lift . readRef
+    writeRef r = lift . writeRef r
+
+
+-- implement NodeGraph instance so that we don't need to keep lifting...
+instance NodeGraph t m => NodeGraph t (AccStateT t f s m) where
+    askParent = lift askParent
+    askPostBuildEvent = lift askPostBuildEvent
+    askRunWithActions = lift askRunWithActions
+    askRunWithActionsAsync = lift askRunWithActionsAsync
+    subGraph n m = AccStateT . StateT $ \ts -> ReaderT $ \d -> subGraph n (_runAccStateT' m d ts)
+    holdGraph n ma emb = do
+      d <- watch
+      ((a, tsZ), erb) <- lift $ holdGraph n (_runAccStateT' ma d []) $ ffor emb $ \mb -> _runAccStateT' mb d []
+      let et = (mergeWith composeMaybe . snd) <$> erb
+          tz = mergeWith composeMaybe tsZ
+      switchPromptly tz et >>= adjustMaybe
+      return (a, fst <$> erb)
+    buildEvent em = mdo
+      d <- watch
+      built <- lift . buildEvent $ flip pushAlways em $ \m -> do
+                    t <- sample behT
+                    return $ _runAccStateT' m d [t]
+      let et = (mergeWith composeMaybe . snd) <$> built
+      behT :: Behavior t (Event t (s -> Maybe s)) <- hold (never :: Event t (s -> Maybe s)) et
+      adjustMaybe $ switch behT
+      return $ fst <$> built
+    buildEvent_ = void . buildEvent
+    runEventMaybe = lift . runEventMaybe
+    runEvent_ = lift . runEvent_
+    runEvent = lift . runEvent
+
+---- Helpers
+
+-- | partial non: useful to convert a Prism to something that takes a default
+-- e.g., pnon _Just 20 is similar to non 20
+-- NOTE: this isn't an Iso, because the conversion from a to s is impossible when a == def
+-- that means e.g., Nothing ^. pnon _Just 3 = 3; review (pnon _Just 3) 3 != Nothing
+pnon' :: APrism' s a -> a -> Lens' s a
+pnon' p def = lens (fromMaybe def . preview (clonePrism p)) (const (review (clonePrism p)))
+
+-- non without the need for Eq (and doesn't reset source to Nothing when default is supplied)
+pnon :: a -> Lens' (Maybe a) a
+pnon = pnon' _Just
