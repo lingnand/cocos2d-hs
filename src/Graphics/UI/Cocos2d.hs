0002b9c8c4eecebbe699c9474f0e2e96f2aac483
Lingnan Dai <lingnan.d@gmail.com>
2017-03-07 19:41:09 +0000

    Changes on build; add a top level module that takes in all and adds hoppy-runtime dep

---
+++ src/Graphics/UI/Cocos2d.hs
@@ -0,0 +1,21 @@
+module Graphics.UI.Cocos2d
+    (
+      module X
+    )
+  where
+
+
+import Graphics.UI.Cocos2d.Audio as X
+import Graphics.UI.Cocos2d.CocoStudio as X
+import Graphics.UI.Cocos2d.Common as X
+import Graphics.UI.Cocos2d.Director as X
+import Graphics.UI.Cocos2d.Event as X
+import Graphics.UI.Cocos2d.Label as X
+import Graphics.UI.Cocos2d.Layer as X
+import Graphics.UI.Cocos2d.Node as X
+import Graphics.UI.Cocos2d.Scene as X
+import Graphics.UI.Cocos2d.Sprite as X
+import Graphics.UI.Cocos2d.Std as X
+import Graphics.UI.Cocos2d.Texture as X
+import Graphics.UI.Cocos2d.Widget as X
+import Foreign.Hoppy.Runtime as X
